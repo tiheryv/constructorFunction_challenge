199b0018004edfb959ec1f21dd53cb258136e8df
tiheryv <tiheryvr@gmail.com>
2021-08-11 13:41:54 -0500

    Added constructor and collection of products

---
+++ js/koreanGroceryStore.js
@@ -0,0 +1,559 @@
+const koreanProducts = [
+    {
+        name: 'Kimchi',
+        description: 'Traditional Korean dish',
+        cost: 80.22,
+        clasification: 'Food',
+        profitPercentage: 15,
+        monthlyPrices:[
+            {
+                month: 1,
+                price: 78.20
+            },
+            {
+                month: 2,
+                price: 81.50
+            },
+            {
+                month: 3,
+                price: 75.90
+            },
+            {
+                month: 4,
+                price: 80.75
+            },
+            {
+                month: 5,
+                price: 80.05
+            },
+            {
+                month: 6,
+                price: 79.66
+            },
+        ],
+        weeklySales: [
+            {
+                day: 1,
+                quantity: 8
+            },
+            {
+                day: 2,
+                quantity: 6
+            },
+            {
+                day: 3,
+                quantity: 5
+            },
+            {
+                day: 4,
+                quantity: 5
+            },
+            {
+                day: 5,
+                quantity: 9
+            },
+            {
+                day: 6,
+                quantity: 7
+            },
+            {
+                day: 7,
+                quantity: 8
+            },
+        ], 
+        expirationDate: '2021/08/15'
+    },
+    {
+        name: 'Soju',
+        description: 'Distilled alcoholic beverage',
+        cost: 50.62,
+        clasification: 'Drinks',
+        profitPercentage: 20,
+        monthlyPrices:[
+            {
+                month: 1,
+                price: 48
+            },
+            {
+                month: 2,
+                price: 49.50
+            },
+            {
+                month: 3,
+                price: 48.22
+            },
+            {
+                month: 4,
+                price: 50.75
+            },
+            {
+                month: 5,
+                price: 50.05
+            },
+            {
+                month: 6,
+                price: 48.33
+            },
+        ],
+        weeklySales: [
+            {
+                day: 1,
+                quantity: 2
+            },
+            {
+                day: 2,
+                quantity: 0
+            },
+            {
+                day: 3,
+                quantity: 1
+            },
+            {
+                day: 4,
+                quantity: 1
+            },
+            {
+                day: 5,
+                quantity: 3
+            },
+            {
+                day: 6,
+                quantity: 5
+            },
+            {
+                day: 7,
+                quantity: 1
+            },
+        ], 
+        expirationDate: '2022/02/21'
+    },
+    {
+        name: 'Instant rice',
+        description: 'Steamed rice',
+        cost: 35.10,
+        clasification: 'Food',
+        profitPercentage: 20,
+        monthlyPrices:[
+            {
+                month: 1,
+                price: 35.20
+            },
+            {
+                month: 2,
+                price: 33.50
+            },
+            {
+                month: 3,
+                price: 35.90
+            },
+            {
+                month: 4,
+                price: 35.75
+            },
+            {
+                month: 5,
+                price: 36.05
+            },
+            {
+                month: 6,
+                price: 34.66
+            },
+        ],
+        weeklySales: [
+            {
+                day: 1,
+                quantity: 10
+            },
+            {
+                day: 2,
+                quantity: 6
+            },
+            {
+                day: 3,
+                quantity: 8
+            },
+            {
+                day: 4,
+                quantity: 3
+            },
+            {
+                day: 5,
+                quantity: 4
+            },
+            {
+                day: 6,
+                quantity: 4
+            },
+            {
+                day: 7,
+                quantity: 6
+            },
+        ], 
+        expirationDate: '2021/08/17'
+    },
+    {
+        name: 'Pocky',
+        description: 'Sweet snack food covered in chocolate',
+        cost: 10.22,
+        clasification: 'Snack',
+        profitPercentage: 15,
+        monthlyPrices:[
+            {
+                month: 1,
+                price: 9.20
+            },
+            {
+                month: 2,
+                price: 11.50
+            },
+            {
+                month: 3,
+                price: 11.90
+            },
+            {
+                month: 4,
+                price: 10.35
+            },
+            {
+                month: 5,
+                price: 10.99
+            },
+            {
+                month: 6,
+                price: 9.85
+            },
+        ],
+        weeklySales: [
+            {
+                day: 1,
+                quantity: 5
+            },
+            {
+                day: 2,
+                quantity: 2
+            },
+            {
+                day: 3,
+                quantity: 2
+            },
+            {
+                day: 4,
+                quantity: 3
+            },
+            {
+                day: 5,
+                quantity: 4
+            },
+            {
+                day: 6,
+                quantity: 6
+            },
+            {
+                day: 7,
+                quantity: 1
+            },
+        ], 
+        expirationDate: '2021/09/15'
+    },
+    {
+        name: 'Instant ramen',
+        description: 'Instant floavored ramen',
+        cost: 5.72,
+        clasification: 'Food',
+        profitPercentage: 70,
+        monthlyPrices:[
+            {
+                month: 1,
+                price: 4.20
+            },
+            {
+                month: 2,
+                price: 5.75
+            },
+            {
+                month: 3,
+                price: 3.90
+            },
+            {
+                month: 4,
+                price: 5.75
+            },
+            {
+                month: 5,
+                price: 5.05
+            },
+            {
+                month: 6,
+                price: 7.66
+            },
+        ],
+        weeklySales: [
+            {
+                day: 1,
+                quantity: 12
+            },
+            {
+                day: 2,
+                quantity: 8
+            },
+            {
+                day: 3,
+                quantity: 10
+            },
+            {
+                day: 4,
+                quantity: 10
+            },
+            {
+                day: 5,
+                quantity: 9
+            },
+            {
+                day: 6,
+                quantity: 7
+            },
+            {
+                day: 7,
+                quantity: 8
+            },
+        ], 
+        expirationDate: '2021/08/22'
+    },
+    {
+        name: 'Spicy Chips',
+        description: 'Spicy snack, very very spicy',
+        cost: 20.89,
+        clasification: 'Snack',
+        profitPercentage: 15,
+        monthlyPrices:[
+            {
+                month: 1,
+                price: 19.20
+            },
+            {
+                month: 2,
+                price: 19.50
+            },
+            {
+                month: 3,
+                price: 21.90
+            },
+            {
+                month: 4,
+                price: 20.75
+            },
+            {
+                month: 5,
+                price: 20.05
+            },
+            {
+                month: 6,
+                price: 18
+            },
+        ],
+        weeklySales: [
+            {
+                day: 1,
+                quantity: 3
+            },
+            {
+                day: 2,
+                quantity: 8
+            },
+            {
+                day: 3,
+                quantity: 1
+            },
+            {
+                day: 4,
+                quantity: 0
+            },
+            {
+                day: 5,
+                quantity: 6
+            },
+            {
+                day: 6,
+                quantity: 2
+            },
+            {
+                day: 7,
+                quantity: 4
+            },
+        ], 
+        expirationDate: '2021/11/15'
+    },
+    {
+        name: 'Nori seaweed',
+        description: 'Dried edible seaweed',
+        cost: 8.12,
+        clasification: 'Seasoning',
+        profitPercentage: 50,
+        monthlyPrices:[
+            {
+                month: 1,
+                price: 8.15
+            },
+            {
+                month: 2,
+                price: 8.56
+            },
+            {
+                month: 3,
+                price: 7.99
+            },
+            {
+                month: 4,
+                price: 8.25
+            },
+            {
+                month: 5,
+                price: 8
+            },
+            {
+                month: 6,
+                price: 7
+            },
+        ],
+        weeklySales: [
+            {
+                day: 1,
+                quantity: 4
+            },
+            {
+                day: 2,
+                quantity: 9
+            },
+            {
+                day: 3,
+                quantity: 7
+            },
+            {
+                day: 4,
+                quantity: 7
+            },
+            {
+                day: 5,
+                quantity: 3
+            },
+            {
+                day: 6,
+                quantity: 10
+            },
+            {
+                day: 7,
+                quantity: 9
+            },
+        ], 
+        expirationDate: '2021/08/29'
+    },
+    {
+        name: 'Makgeolli',
+        description: 'Traditional Korean dish',
+        cost: 84.99,
+        clasification: 'Drinks',
+        profitPercentage: 25,
+        monthlyPrices:[
+            {
+                month: 1,
+                price: 85.20
+            },
+            {
+                month: 2,
+                price: 84.77
+            },
+            {
+                month: 3,
+                price: 86.10
+            },
+            {
+                month: 4,
+                price: 84.75
+            },
+            {
+                month: 5,
+                price: 83.79
+            },
+            {
+                month: 6,
+                price: 84.66
+            },
+        ],
+        weeklySales: [
+            {
+                day: 1,
+                quantity: 2
+            },
+            {
+                day: 2,
+                quantity: 0
+            },
+            {
+                day: 3,
+                quantity: 0
+            },
+            {
+                day: 4,
+                quantity: 1
+            },
+            {
+                day: 5,
+                quantity: 3
+            },
+            {
+                day: 6,
+                quantity: 3
+            },
+            {
+                day: 7,
+                quantity: 1
+            },
+        ], 
+        expirationDate: '2021/05/09'
+    },
+]
+
+function Product (name, description, cost, clasification, profitPercentage, monthlyPrices, weeklySales,expirationDate){
+    this.name = name
+    this.description = description
+    this.cost = cost
+    this.clasification = clasification
+    this.profitPercentage = profitPercentage
+    this.monthlyPrices = monthlyPrices
+    this.weeklySales = weeklySales
+    this.expirationDate = expirationDate
+    this.salePrice = 0
+    this.daysRemain = function(){
+        let today = new Date()
+        let expirationDate = new Date(this.expirationDate)
+
+        let monthsRemain = expirationDate.getMonth() - today.getMonth()
+        console.log(monthsRemain)
+        let daysRemain = expirationDate.getDate() - today.getDate()
+        return `Quedan: ${monthsRemain} meses y ${daysRemain} dias `
+    }
+    this.priceAverage = function(){
+        let total = this.monthlyPrices.reduce( (accum, current) => {
+
+            let price = current.price
+            return accum + price
+        },0)
+
+        
+        return (total / 6).toFixed(2)
+    }
+}
+
+
+let koreanProductsArray = koreanProducts.map( item => new Product(item.name, item.description, item.cost, item.clasification, item.profitPercentage, item.monthlyPrices, item.weeklySales, item.expirationDate))
+console.log(koreanProductsArray)
+
+const getSalePrice = (item => {
+    let percentage = (item.profitPercentage / 100) * item.cost 
+    return (percentage + item.cost).toFixed(2)
+})
+
+koreanProductsArray.forEach(element => {
+    let salePrice = getSalePrice(element)
+    element.salePrice = parseFloat(salePrice)
+})
